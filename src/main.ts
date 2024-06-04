ca0975e8f819df62b2339a140e4a0ca21ba44684
EralChen <eralchen@foxmail.com>
2024-06-04 10:19:29 +0800

    feat: demoContainerPlugin with highlight

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'vitepress/dist/client/theme-default/styles/base.css'
 import 'vitepress/dist/client/theme-default/styles/vars.css'
-import 'vitepress/dist/client/theme-default/styles/components/custom-block.css'
+import 'vitepress/dist/client/theme-default/styles/icons.css'
+import 'vitepress/dist/client/theme-default/styles/components/vp-doc.css'
+import 'vitepress/dist/client/theme-default/styles/components/vp-code.css'
 import 'element-plus/dist/index.css'
 createApp(App).mount('#app')
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import md from 'unplugin-vue-markdown/vite'
-import { demoContainerPlugin, DemoContainerPluginSettings } from '@vunk/shared/markdown/plugins'
+import { demoContainerPlugin, DemoContainerPluginSettings, copyableFencePlugin } from '@vunk/shared/markdown/plugins'
+import { highlight } from '@vunk/shared/markdown/shiki'
 import path from 'path'
 
 // https://vitejs.dev/config/
@@ -16,12 +17,22 @@ export default defineConfig({
     }),
     vueJsx(),
     md({
+      markdownItOptions: {
+        highlight: (await highlight({
+          dark: 'github-dark',
+          light: 'github-light',
+        }, {})),
+      },
       markdownItSetup (mdit) {
+        mdit.use(copyableFencePlugin)
         mdit.use(demoContainerPlugin, {
           root: path.resolve(__dirname, './src/demos'),
           globSource: '**/*.vue'
         } as DemoContainerPluginSettings)
       },
+      wrapperClasses: [
+        'vp-doc'
+      ]
     }),
     VueDevTools(),
   ],
